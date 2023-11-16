ce270e03bc94af4fcec7ae7448b7f57bfc9f4a97
anhTuspham <panhtu0902@gmail.com>
2023-11-16 17:58:52 +0700

    feat(product): handle show attribute select for single variation

--- core/assets/js/posts-type/product/ProductPost.js
+++ core/assets/js/posts-type/product/ProductPost.js
@@ -19,11 +19,19 @@ export default class ProductPost {
 
             // input
             attributesName: wrapper.querySelector('[data-attribute-name]'),
-            productTypeValue: wrapper.querySelector('[data-productType-value]')
+            productTypeValue: wrapper.querySelector('[data-productType-value]'),
+
+            // textarea json
+            jsonElement: wrapper.querySelector('[data-product-json]')
+
         }
 
         this.attributeIndex = 1;
         this.variationIndex = 0;
+
+        // vars
+        const urlObject = new URL(location.href);
+        this.FETCH_URL = urlObject.origin + urlObject.pathname;
 
         this.wrapper.addEventListener('click', this.handleWrapperClick.bind(this))
     }
@@ -81,9 +89,76 @@ export default class ProductPost {
         taxonomyAttribute.remove()
     }
 
+    splitString = (currentString, character = ' ') => {
+        return currentString.split(character).map(value => value.trim())
+    }
+
+    /**
+     * Save attribute
+     * */
+    handleSaveAttribute() {
+        let attribute = {}
+        let attributeName = [];
+        let attributeValue = [];
+        this.wrapper.querySelectorAll('[data-attribute-product-name] input').forEach(input => {
+            attributeName.push(input.value)
+        })
+        this.wrapper.querySelectorAll('[data-attribute-product-value] textarea').forEach(textarea => {
+            attributeValue.push(textarea.value);
+        })
+
+        for(let i = 0; i < attributeName.length; i++){
+            attribute[attributeName[i]] = this.splitString(attributeValue[i], '|')
+        }
+
+        const product = {
+            attribute: []
+        };
+
+        for (let key in attribute){
+            product.attribute.push({
+                name: key,
+                value: attribute[key]
+            })
+        }
+
+        console.log('product', product)
+        const data = JSON.stringify(product);
+        console.log('data', data)
+        this.elements.jsonElement.textContent = data
+        const dataParse = JSON.parse(data)
+        console.log('parse: ', dataParse)
+
+    }
+
+    /**
+     * Add variation manually
+     * */
+    handleAddVariation() {
+
+    }
+
     handleAddNewVariation() {
+        // when attribute doesn't save can create new variation
+        if(!this.elements.jsonElement.textContent) return
+
+        const productElement = JSON.parse(this.elements.jsonElement.textContent)
+
+        let selectHTML = '';
+        for(let i = 0; i < productElement.attribute.length; i++){
+            selectHTML +=
+                `    <label for="${productElement.attribute[i].name}">${productElement.attribute[i].name}</label>
+                     <select data-easy-select name="attributeName[0]" id="${productElement.attribute[i].name}">`
+            productElement.attribute[i].value.forEach(value => {
+                selectHTML += `<option value="${value}">${value}</option>`
+
+            })
+            selectHTML += `</select>`
+        }
+
         // create child
         const newVariation = document.createElement('div');
+
         newVariation.innerHTML = `
                             <div data-single-variation>
                                 <div>
@@ -92,20 +167,7 @@ export default class ProductPost {
                                 </div>
 
                                 <!-- attributes drop down-->
-                                <div>
-                                    <select data-easy-select name="visibility" id="visibility">
-                                        <option  value="public">Public
-                                        </option>
-                                        <option value="private">Private
-                                        </option>
-                                    </select>
-                                    <select data-easy-select name="visibility" id="visibility">
-                                        <option value="public">Public
-                                        </option>
-                                        <option value="private">Private
-                                        </option>
-                                    </select>
-                                </div>
+                                <div>${selectHTML}</div>
 
                                 <!-- variation detail -->
                                 <div data-variation-detail class="closed">
@@ -139,7 +201,7 @@ export default class ProductPost {
         newVariation.setAttribute('data-variation-number', this.variationIndex)
 
         // replace input, textarea name by the increasing index
-        newVariation.querySelectorAll('input, textarea').forEach(
+        newVariation.querySelectorAll('select, input, textarea').forEach(
             input => {
                 input.name = input.name.replace(/\[\d+]/, '[' + this.variationIndex + ']');
 
@@ -155,20 +217,19 @@ export default class ProductPost {
      * Handle toggle show single variation
      * */
     handleToggleVariationDetails(target) {
-        console.log(target)
         const singleVariationEl = target.closest('[data-single-variation]');
 
-        const singleVariationDetailEl = singleVariationEl.querySelector('[data-variation-detail]')
+        const singleVariationDetailEl = singleVariationEl.querySelector('[data-variation-detail]');
 
-        if(singleVariationDetailEl) singleVariationDetailEl.classList.toggle('closed')
+        if(singleVariationDetailEl) singleVariationDetailEl.classList.toggle('closed');
     }
 
     /**
-     *
+     * Delete variation
      * */
     handleDeleteVariation(target) {
-        const variationEl = target.closest('[data-variation-number]')
-        if(variationEl) variationEl.remove()
+        const variationEl = target.closest('[data-variation-number]');
+        if(variationEl) variationEl.remove();
     }
 
     /**
@@ -176,11 +237,12 @@ export default class ProductPost {
      * */
     switchProductTypeOptions() {
 
-        // If the selected value of the productType is a variable, then display the option to show attributes.
+        // If the selected value of the productType is a 'variable', then display the option to show attributes.
         const isProductTypeVariable = this.elements.productTypeValue.options[this.elements.productTypeValue.selectedIndex].value === 'variable';
 
-        this.elements.variableProduct.style.display = isProductTypeVariable ? 'block' : 'none';
         this.elements.simpleProduct.style.display = isProductTypeVariable ? 'none' : 'block';
+
+        this.elements.variableProduct.style.display = isProductTypeVariable ? 'block' : 'none';
         this.elements.variationProductDOM.style.display = isProductTypeVariable ? 'block' : 'none';
     }
 
@@ -192,38 +254,59 @@ export default class ProductPost {
         }
         let target = null;
 
+        // attribute
         const addNewAttributeBtnEl = e.target.closest('button[data-add-new-attribute]');
         const deleteAttributeBtnEl = e.target.closest('button[data-delete-attribute]');
         const attributeNameEl = e.target.closest('[data-attribute-name]');
+        const saveAttributeBtnEl = e.target.closest('[data-save-attribute]');
 
+        // variation
         const addNewVariationBtnEl = e.target.closest('button[data-add-new-variation]');
         const showVariationDetailEl = e.target.closest('[data-variation-name]');
         const deleteVariationBtnEl = e.target.closest('button[data-delete-variation]');
 
 
+        // add new attribute
         if (addNewAttributeBtnEl) {
             functionHandling = this.handleAddNewAttribute.bind(this);
+        }
 
-        } else if (deleteAttributeBtnEl) {
+        // delete attribute
+        else if (deleteAttributeBtnEl) {
             functionHandling = this.handleDeleteAttribute.bind(this);
             target = deleteAttributeBtnEl
+        }
 
-        } else if(attributeNameEl) {
+        // show detail attribute
+        else if(attributeNameEl) {
             functionHandling = this.handleToggleAttributeDetails.bind(this);
-            target = attributeNameEl
+            target = attributeNameEl;
+        }
 
-        } else if(addNewVariationBtnEl) {
+        // save attribute
+        else if(saveAttributeBtnEl) {
+            functionHandling = this.handleSaveAttribute.bind(this);
+        }
+
+        // add new variation
+        else if(addNewVariationBtnEl) {
             functionHandling = this.handleAddNewVariation.bind(this);
+        }
 
-        } else if(showVariationDetailEl) {
+        // show detail variation
+        else if(showVariationDetailEl) {
             functionHandling = this.handleToggleVariationDetails.bind(this);
             target = showVariationDetailEl
+        }
 
-        } else if(deleteVariationBtnEl) {
+        // delete variation
+        else if(deleteVariationBtnEl) {
             functionHandling = this.handleDeleteVariation.bind(this);
             target = deleteVariationBtnEl;
+        }
 
-        } else {
+        // switching simple product and variable product
+        else {
             functionHandling = this.switchProductTypeOptions.bind(this);
         }
 
