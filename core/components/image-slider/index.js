5d623e464c2e203d69de4b63981d17342c3bc953
vuquangpham <vuquangpham2909@gmail.com>
2023-11-16 13:54:13 +0700

    feat: introduce image slider

---
+++ core/components/image-slider/index.js
@@ -0,0 +1,54 @@
+const ComponentController = require('../../classes/component/component-controller');
+const Component = require('../../classes/component/component');
+const Media = require('../../categories/media');
+
+class ImageSlider extends Component{
+    constructor(){
+        super({
+            name: 'image-slider',
+            title: 'Image Slider',
+            description: 'WYSIWYG content and images',
+            params: [
+                {
+                    type: ComponentController.paramTypes.TEXT,
+                    heading: 'Content',
+                    paramName: 'content',
+                    classesName: 'col-8',
+                    description: 'WYSIWYG Content'
+                },
+                {
+                    type: ComponentController.paramTypes.IMAGE,
+                    heading: 'Image',
+                    paramName: 'images',
+                    classesName: 'col-4',
+                },
+            ],
+        });
+    }
+
+    async render(data){
+        const params = data.params;
+        const options = data.options;
+
+        // content
+        const content = this.getParam(params, 'content');
+
+        // image
+        const imagesId = this.getParam(params, 'images');
+        const promises = imagesId.map(id => Media.getDataById(id));
+
+        const images = await Promise.all(promises);
+        const imagesHTML = images.map(i => `
+<div class="image-slider__image img-wrapper-cover skeleton-bg">
+    <img src="${i.url.original}" alt="${i.name}" />
+</div>`).join('');
+
+        return `
+<div class="image-slider">
+    <div class="image-slider__content">${content}</div>
+    <div class="image-slider__images">${imagesHTML}</div>
+</div>`;
+    }
+}
+
+module.exports = new ImageSlider();
