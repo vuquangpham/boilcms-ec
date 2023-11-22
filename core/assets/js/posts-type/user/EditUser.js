ce741703c467d70b72d706eae496fc00d665f747
vuquangpham <vuquangpham2909@gmail.com>
2023-11-23 00:23:36 +0700

    refactor(popup): change edit user popup

--- core/assets/js/posts-type/user/EditUser.js
+++ core/assets/js/posts-type/user/EditUser.js
@@ -1,14 +1,13 @@
 import fetch from "@global/js/fetch";
 
-export default class EditUser {
-    constructor(wrapper) {
-        this.wrapper = wrapper
+export default class EditUser{
+    constructor(wrapper){
+        this.wrapper = wrapper;
 
         this.elements = {
 
             // popup
             popupForm: wrapper.querySelector('[data-user-form]'),
-            closePopupForm: wrapper.querySelector('[data-user-close-btn]'),
 
             // input fields
             userNameInput: wrapper.querySelector('[data-user-name]'),
@@ -21,7 +20,7 @@ export default class EditUser {
             selectStateInput: wrapper.querySelector('[data-select-state-value]'),
             optionStateInput: wrapper.querySelectorAll('[data-select-state-value] option'),
 
-        }
+        };
 
         // vars
         const urlObject = new URL(location.href);
@@ -29,6 +28,35 @@ export default class EditUser {
 
         // handle click action
         this.wrapper.addEventListener('click', this.handleWrapperClick.bind(this));
+
+        // init Popup
+        this.editUserPopup = Popup.create({
+            target: document.createElement('div'),
+            popupContent: this.wrapper.querySelector('[data-popup-content="edit-user-page"]'),
+
+            onPopupContentClick: (self) => {
+                const eventTarget = self.event.target;
+
+                let functionHandling = () => {
+                };
+                let target = null;
+
+                const saveUserBtnEl = eventTarget.closest('button[data-user-save-btn]');
+                const deleteUserBtnEl = eventTarget.closest('button[data-user-delete-btn]');
+
+                if(saveUserBtnEl){
+                    functionHandling = this.handleSaveUser.bind(this);
+                    target = saveUserBtnEl;
+
+                }else if(deleteUserBtnEl){
+                    functionHandling = this.handleDeleteUser.bind(this);
+                    target = deleteUserBtnEl;
+                }
+
+                // call the function
+                functionHandling(target);
+            }
+        });
     }
 
     // modify date publish
@@ -44,7 +72,7 @@ export default class EditUser {
     /**
      * Replace user item in popup
      * */
-    replaceUser(data) {
+    replaceUser(data){
         // set the id for the form
         this.elements.popupForm.dataset.id = data._id;
 
@@ -52,26 +80,29 @@ export default class EditUser {
         this.elements.userNameInput.value = data.name;
         this.elements.userEmailInput.value = data.email;
         this.elements.userRegisterInput.textContent = this.modifyDate(new Date(data.registerAt));
-        this.elements.selectRoleInput.value = data.role
+        this.elements.selectRoleInput.value = data.role;
 
         this.elements.optionRoleInput.forEach(o => {
-            if (o.getAttribute('value') === data.role) {
-                o.setAttribute('selected', '')
-            } else o.removeAttribute('selected')
-        })
+            if(o.getAttribute('value') === data.role){
+                o.setAttribute('selected', '');
+            }else o.removeAttribute('selected');
+        });
 
         this.elements.optionStateInput.forEach(o => {
-            if (o.getAttribute('value') === data.state) {
-                o.setAttribute('selected', '')
-            } else o.removeAttribute('selected')
-        })
+            if(o.getAttribute('value') === data.state){
+                o.setAttribute('selected', '');
+            }else o.removeAttribute('selected');
+        });
 
     };
 
     /**
      * Show single user item
      * */
-    showSingleUser(target) {
+    showSingleUser(target){
+        // open popup
+        this.editUserPopup.open();
+
         const formEl = target.closest('[data-user-item]');
         const id = formEl.getAttribute('data-id');
 
@@ -85,17 +116,17 @@ export default class EditUser {
         })
             .then(res => res.json())
             .then(result => {
-                this.replaceUser(result.data)
+                this.replaceUser(result.data);
             })
 
             // catch the error
-            .catch(err => console.error(err))
+            .catch(err => console.error(err));
     };
 
     /**
      * Handle save user
      * */
-    handleSaveUser(target) {
+    handleSaveUser(target){
         const formEl = target.closest('[data-user-form]');
         const id = formEl.getAttribute('data-id');
 
@@ -103,7 +134,7 @@ export default class EditUser {
         formData.append('name', this.elements.userNameInput.value);
         formData.append('email', this.elements.userEmailInput.value);
         formData.append('role', this.elements.selectRoleInput.value);
-        formData.append('state', this.elements.selectStateInput.value)
+        formData.append('state', this.elements.selectStateInput.value);
 
         fetch(this.FETCH_URL, {
             method: 'post',
@@ -118,23 +149,23 @@ export default class EditUser {
             .then((result) => {
 
                 const userItemEl = this.wrapper.querySelector(`[data-user-item][data-id="${id}"]`);
-                if (!userItemEl) {
+                if(!userItemEl){
                     console.error('Can not find an image with id', id);
                     return;
                 }
 
                 // update the new user
-                userItemEl.querySelector('[data-user-item-name]').innerHTML = result.name
-                userItemEl.querySelector('[data-user-item-email]').innerHTML = result.email
-                userItemEl.querySelector('[data-user-item-role]').innerHTML = result.role
-                userItemEl.querySelector('[data-user-item-state] span').innerHTML = result.state
+                userItemEl.querySelector('[data-user-item-name]').innerHTML = result.name;
+                userItemEl.querySelector('[data-user-item-email]').innerHTML = result.email;
+                userItemEl.querySelector('[data-user-item-role]').innerHTML = result.role;
+                userItemEl.querySelector('[data-user-item-state] span').innerHTML = result.state;
 
                 // add class for any state
                 result.state === 'active' ? userItemEl.querySelector('[data-user-item-state] span').className = 'badge badge--green' :
-                    userItemEl.querySelector('[data-user-item-state] span').className = 'badge badge--red'
+                    userItemEl.querySelector('[data-user-item-state] span').className = 'badge badge--red';
 
                 // close the popup
-                this.elements.closePopupForm.click();
+                this.editUserPopup.close();
             })
             .catch(err => console.error(err));
     }
@@ -142,7 +173,7 @@ export default class EditUser {
     /**
      * Handle delete user
      * */
-    handleDeleteUser(target) {
+    handleDeleteUser(target){
         const formEl = target.closest('[data-user-form]');
         const id = formEl.getAttribute('data-id');
 
@@ -155,36 +186,26 @@ export default class EditUser {
             .then(() => {
                 // get deleted user item and remove the dom
                 const deletedUserItem = this.wrapper.querySelector(`[data-user-item][data-id="${id}"]`);
-                if (deletedUserItem) {
+                if(deletedUserItem){
                     deletedUserItem.remove();
                 }
 
                 // close the popup
-                this.elements.closePopupForm.click();
-            })
+                this.editUserPopup.close();
+            });
     }
 
 
-    handleWrapperClick(e) {
+    handleWrapperClick(e){
         let functionHandling = () => {
         };
         let target = null;
 
         const singleUserItemEl = e.target.closest('button[data-user-edit]');
-        const saveUserBtnEl = e.target.closest('button[data-user-save-btn]')
-        const deleteUserBtnEl = e.target.closest('button[data-user-delete-btn]')
 
-        if (singleUserItemEl) {
-            functionHandling = this.showSingleUser.bind(this)
-            target = singleUserItemEl
-
-        } else if (saveUserBtnEl) {
-            functionHandling = this.handleSaveUser.bind(this);
-            target = saveUserBtnEl
-
-        } else if (deleteUserBtnEl) {
-            functionHandling = this.handleDeleteUser.bind(this);
-            target = deleteUserBtnEl
+        if(singleUserItemEl){
+            functionHandling = this.showSingleUser.bind(this);
+            target = singleUserItemEl;
         }
 
         // call the function
