c0a1ee7c6385b812b34abed6f484cfa577d9f833
vuquangpham <vuquangpham2909@gmail.com>
2023-11-25 00:39:33 +0700

    feat(product-detail): handle switching between variation

--- app/js/index.js
+++ app/js/index.js
@@ -4,6 +4,7 @@ import '../styles/index.scss';
 // vendors
 import '@global/vendors/theme/theme.min.js';
 import '@global/vendors/accordion/Accordion.min.js';
+import '@global/vendors/easy-select/easy-select.min.js';
 
 import '@global/vendors/popup/Popup.min.js';
 import '@global/vendors/popup/Popup.min.css';
--- app/js/pages/products.js
+++ app/js/pages/products.js
@@ -1,30 +1,70 @@
 import '../vendors/flickity';
 
-document.querySelectorAll('body.product-detail-page .product-detail__inner').forEach(wrapper => {
+document.querySelectorAll('body.product-detail-page .product-detail').forEach(wrapper => {
+    // selected attributes
+    const selectedAttributes = [];
 
-    // init slider
-    const mainSlider = wrapper.querySelector('.product-detail__images');
-    new Flickity(mainSlider, {
-        // options
-        contain: true,
-        pageDots: false,
-        prevNextButtons: true,
-        groupCells: '100%',
-        wrapAround: true,
-        fade: true
-    });
+    // handle on change
+    const handleVariationChange = (currentProduct) => {
+        const attributesValues = Array.from(currentProduct.querySelectorAll('[data-product-attribute-select]'))
+            .map(select => {
+                const name = select.getAttribute('name');
+                const value = select.value;
 
-    // nav slider
-    const navSlider = wrapper.querySelector('.product-detail__images-nav');
-    if(navSlider){
-        new Flickity(navSlider, {
+                return {
+                    name, value
+                };
+            });
+
+        // compare with the selected attributes
+        selectedAttributes.find(selectedAttribute => {
+            const active = selectedAttribute.every(s => !!attributesValues.find(a => a.name === s.name && a.value === s.value));
+            console.log('active', active);
+        });
+
+        console.log(attributesValues);
+        console.log(selectedAttributes);
+    };
+
+    // inner element
+    wrapper.querySelectorAll('.product-detail__inner').forEach(inner => {
+        // init slider
+        const mainSlider = inner.querySelector('.product-detail__images');
+        new Flickity(mainSlider, {
             // options
             contain: true,
             pageDots: false,
-            asNavFor: mainSlider,
-            prevNextButtons: false,
-            wrapAround: false,
-            cellAlign: 'left'
+            prevNextButtons: true,
+            groupCells: '100%',
+            wrapAround: true,
+            fade: true
         });
-    }
+
+        // nav slider
+        const navSlider = inner.querySelector('.product-detail__images-nav');
+        if(navSlider){
+            new Flickity(navSlider, {
+                // options
+                contain: true,
+                pageDots: false,
+                asNavFor: mainSlider,
+                prevNextButtons: false,
+                wrapAround: false,
+                cellAlign: 'left'
+            });
+        }
+
+        // register selected attribute
+        const selectedAttribute = JSON.parse(inner.getAttribute('data-selected-attributes'));
+        selectedAttributes.push(selectedAttribute);
+
+        // init easy select
+        inner.querySelectorAll('[data-product-attribute-select]').forEach(e => {
+            EasySelect.init(e, {
+                onChange: (self) => {
+                    handleVariationChange(inner);
+                }
+            });
+        });
+    });
 });
