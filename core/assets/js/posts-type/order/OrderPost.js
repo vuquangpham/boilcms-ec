269620b31339a4eeafaca8f450f232979c9e0acf
anhTuspham <panhtu0902@gmail.com>
2023-12-03 21:08:42 +0700

    feat(order): handle edit popup (not done)

--- core/assets/js/posts-type/order/OrderPost.js
+++ core/assets/js/posts-type/order/OrderPost.js
@@ -15,10 +15,16 @@ export default class OrderPost{
             emailInput: wrapper.querySelector('[data-email]'),
             phoneInput: wrapper.querySelector('[data-phone]'),
             shippingFeeInput: wrapper.querySelector('[data-shipping-fee]'),
+            totalInput: wrapper.querySelector('[data-total]'),
 
             // select input
             paymentSelectInput: wrapper.querySelector('[data-select-payment-value]'),
             paidSelectInput: wrapper.querySelector('[data-select-paid-value]'),
+
+            // product list input
+            productImageInput: wrapper.querySelector('[data-product-image-item] img'),
+            productNameInput: wrapper.querySelector('[data-product-name-item]'),
+            productQuantityInput: wrapper.querySelector('[data-product-quantity-item]'),
         };
 
         // vars
@@ -31,7 +37,25 @@ export default class OrderPost{
         // create popup for each [data-order-edit] element
         this.editOrderPopup = Popup.create({
             target: document.createElement('div'),
-            popupContent: this.wrapper.querySelector('[data-popup-content="data-order-edit"]')
+            popupContent: this.wrapper.querySelector('[data-popup-content="data-order-edit"]'),
+
+            onPopupContentClick: (self) => {
+                const eventTarget = self.event.target;
+
+                let functionHandling = () => {
+                };
+                let target = null;
+
+                const saveOrderBtnEl = eventTarget.closest('button[data-order-update-btn]');
+
+                if(saveOrderBtnEl) {
+                    functionHandling = this.handleSaveOrder.bind(this);
+                    target = saveOrderBtnEl;
+
+                    // call the function
+                    functionHandling(target);
+                }
+            }
 
         })
 
@@ -46,26 +70,20 @@ export default class OrderPost{
 
         // change the input of the form
         this.elements.orderID.textContent = data.orderID;
-        this.elements.usernameInput.value = data.fullName;
-        this.elements.addressInput.value = data.address;
-        this.elements.emailInput.value = data.user.email;
-        this.elements.phoneInput.value = data.phoneNumber;
-        this.elements.shippingFeeInput.value = data.shippingFee
+        this.elements.usernameInput.textContent = data.fullName;
+        this.elements.addressInput.textContent = data.address;
+        this.elements.emailInput.textContent = data.user.email;
+        this.elements.phoneInput.textContent = data.phoneNumber;
+        this.elements.shippingFeeInput.textContent = data.shippingFee;
+        this.elements.totalInput.textContent = data.totalPrice
         this.elements.paymentSelectInput.value = data.paymentMethod;
-        this.elements.paidSelectInput.value = data.isPaid;
+        this.elements.paidSelectInput.value = data.isPaid ? 'paid' : 'unpaid'
 
-        // this.elements.optionRoleInput.forEach(o => {
-        //     if(o.getAttribute('value') === data.role){
-        //         o.setAttribute('selected', '');
-        //     }else o.removeAttribute('selected');
-        // });
-        //
-        // this.elements.optionStateInput.forEach(o => {
-        //     if(o.getAttribute('value') === data.state){
-        //         o.setAttribute('selected', '');
-        //     }else o.removeAttribute('selected');
-        // });
-
+        data.variations.forEach(product => {
+            this.elements.productImageInput.src = product.image.small
+            this.elements.productNameInput.textContent = product.productName;
+            this.elements.productQuantityInput.textContent = product.quantity;
+        })
     };
 
     /**
@@ -88,13 +106,52 @@ export default class OrderPost{
         })
             .then(res => res.json())
             .then(result => {
-                console.log(result.data)
                 this.replaceOrder(result.data)
             })
 
             // catch the error
             .catch(err => console.error(err));
     };
+
+    /**
+     * Handle save order
+     * */
+    handleSaveOrder(target){
+        const formEl = target.closest('[data-order-form]');
+        const id = formEl.getAttribute('data-id');
+
+        const formData = new FormData();
+
+        formData.append('paymentMethod', this.elements.paymentSelectInput.value);
+        formData.append('isPaid', this.elements.paidSelectInput.value);
+
+        fetch(this.FETCH_URL, {
+            method: 'post',
+            action: 'edit',
+            getJSON: true,
+            id: id
+        }, {
+            method: 'post',
+            body: formData
+        })
+            .then(res => res.json())
+            .then((result) => {
+
+                const orderItemEl = this.wrapper.querySelector(`[data-order-item][data-id="${id}"]`);
+                if(!orderItemEl){
+                    console.error('Can not find an order with id', id);
+                    return;
+                }
+
+                // update the new user
+                // orderItemEl.querySelector('[data-select-status-value]').innerHTML = result.status;
+
+
+                // close the popup
+                this.editOrderPopup.close();
+            })
+            .catch(err => console.error(err));
+    }
 
     handleWrapperClick(e){
         let functionHandling = () => {
