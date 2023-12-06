0abc5c338b6c963e68c078ff47d35108a7cbd9da
vuquangpham <vuquangpham2909@gmail.com>
2023-12-07 03:44:33 +0700

    feat(default): get data from db

--- core/categories/default.js
+++ core/categories/default.js
@@ -1,9 +1,66 @@
 const Category = require('../classes/category/category');
 const Type = require("../classes/utils/type");
 
+const Order = require('./order');
+
 class Default extends Category{
     constructor(config){
         super(config);
+    }
+
+    async getAllData(){
+
+        try{
+            // get chart data in year 2023
+            const ordersInYear = await Order.databaseModel.find({
+                publish: {
+                    $gte: new Date(new Date().getFullYear(), 0, 1),
+                    $lte: new Date()
+                }
+            });
+
+            // get chart data in month
+            const ordersInMonth = await Order.databaseModel.find({
+                publish: {
+                    $gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+                    $lte: new Date()
+                }
+            });
+
+            // const pending orders
+            const pendingOrders = await Order.databaseModel.find({
+                $and: [
+                    {
+                        status: {
+                            $not: {
+                                $eq: 'success'
+                            }
+                        }
+                    }, {
+                        status: {
+                            $not: {
+                                $eq: 'failed'
+                            }
+                        }
+                    }
+                ]
+            });
+
+            return {
+                ordersInYear,
+                ordersInMonth,
+                pendingOrders
+            };
+
+        }catch(e){
+            console.log(e);
+
+            return {
+                ordersInYear: [],
+                ordersInMonth: [],
+                pendingOrders: []
+            };
+        }
     }
 }
 
