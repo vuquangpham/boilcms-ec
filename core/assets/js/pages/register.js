b0449e7f456f0014ad0278d7f9dfe47adcdff4af
vuquangpham <vuquangpham2909@gmail.com>
2023-12-06 22:50:30 +0700

    fix(register): change popup

--- core/assets/js/pages/register.js
+++ core/assets/js/pages/register.js
@@ -17,7 +17,6 @@ document.querySelectorAll('body.register').forEach(wrapper => {
     if(!form) return;
 
     const inputs = form.querySelectorAll('[data-password-input], [data-name-input], [data-email-input], [data-confirm-password-input], [data-consent-input]');
-
     form.addEventListener('submit', (e) => {
 
         let hasError = false;
@@ -29,26 +28,25 @@ document.querySelectorAll('body.register').forEach(wrapper => {
             const isConsentField = input.hasAttribute('data-consent-input');
 
             // name input
-            if(isNameInput && validate(input))
+            if(isNameInput && !validate(input))
                 return hasError = true;
 
             // email input
-            if(isEmailInput && validate(input, value => value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)))
-                return hasError = true;
+            if(isEmailInput && !validate(input, value => value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))) return hasError = true;
 
             // password input
-            if(isPasswordInput && validate(input, value => value.match(/^[0-9a-zA-Z\W]{8,}$/)))
+            if(isPasswordInput && !validate(input, value => value.match(/^[0-9a-zA-Z\W]{8,}$/)))
                 return hasError = true;
 
             // confirm password input
-            if(isConfirmPasswordInput && validate(input, value => {
+            if(isConfirmPasswordInput && !validate(input, value => {
                 const passwordField = form.querySelector('[data-password-input]').value;
                 return value === passwordField && value.length;
             }))
                 return hasError = true;
 
             // consent field
-            if(isConsentField && validate(input, _ => input.checked)) return hasError = true;
+            if(isConsentField && !validate(input, _ => input.checked)) return hasError = true;
         });
 
         if(hasError) return e.preventDefault();
