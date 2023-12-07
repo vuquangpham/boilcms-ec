5a27359fef4aadbdfb38bd6e24ea1b5382335e5c
vuquangpham <vuquangpham2909@gmail.com>
2023-12-07 11:13:11 +0700

    fix(chart): bug

--- app/js/pages/account.js
+++ app/js/pages/account.js
@@ -1,8 +1,59 @@
+import fetch from '@global/js/fetch';
+
 document.querySelectorAll('body.account-page').forEach(wrapper => {
-    console.log(wrapper);
+
     // init tab
     Accordion.create({
         target: wrapper.querySelector('[data-tab]'),
         type: 'fade'
+    });
+
+    // elements
+    const form = wrapper.querySelector('[data-form-update-password]');
+
+    const elements = {
+        currentPasswordInput: wrapper.querySelector('[data-current-password]'),
+        passwordInput: wrapper.querySelector('[data-password]'),
+        confirmPasswordInput: wrapper.querySelector('[data-confirm-password]'),
+
+        errorMessage: wrapper.querySelector('[data-error-message]')
+    };
+    const fetchURL = form.getAttribute('action');
+
+    console.log('form', form);
+    // update password
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        console.log('submit');
+        const id = form.getAttribute('data-id');
+
+        const formData = new FormData();
+        formData.append('currentPassword', elements.currentPasswordInput.value);
+        formData.append('password', elements.passwordInput.value);
+        formData.append('confirmPassword', elements.confirmPasswordInput.value);
+
+        fetch(fetchURL, {
+            method: 'post',
+            action: 'edit',
+            getJSON: true,
+            id: id
+        }, {
+            method: 'post',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(result => {
+                if(result.errorMessage){
+                    elements.errorMessage.innerHTML = result.errorMessage;
+                    elements.errorMessage.classList.remove('hidden');
+                    return;
+                }
+
+                elements.errorMessage.innerHTML = 'The password has been updated';
+            })
+            .catch(err => {
+                console.log(err);
+            });
     });
 });
